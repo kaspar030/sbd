518e4c28d7af3e9eb5832436f304d3808c4260ab
Kaspar Schleiser <kaspar@schleiser.de>
2025-09-26 13:29:38 +0200

    feat: FileMap compare
    Signed-off-by: Kaspar Schleiser <kaspar@schleiser.de>

--- src/utils.rs
+++ src/utils.rs
@@ -1,4 +1,4 @@
-use std::collections::HashMap;
+use std::collections::{HashMap, HashSet};
 
 use anyhow::Result;
 use camino::{Utf8Path, Utf8PathBuf};
@@ -23,4 +23,55 @@ pub fn write_all(outpath: &Utf8Path, files: &FileMap, overwrite: bool) -> Result
     }
 
     Ok(())
+}
+
+pub fn compare(
+    filemap: &FileMap,
+    outpath: &Utf8Path,
+) -> Result<(Vec<Utf8PathBuf>, Vec<Utf8PathBuf>, Vec<Utf8PathBuf>)> {
+    use walkdir::WalkDir;
+    let mut missing = Vec::new();
+    let mut differing = Vec::new();
+    let mut extra = Vec::new();
+    let mut all = HashSet::new();
+
+    for entry in WalkDir::new(outpath) {
+        let entry = entry?;
+        if entry.file_type().is_file() {
+            let file_path =
+                Utf8PathBuf::from_path_buf(entry.path().to_path_buf()).map_err(|_| {
+                    anyhow::anyhow!("non-utf8 file path: \"{}\"", entry.path().display())
+                })?;
+
+            all.insert(file_path.clone());
+
+            if let Some(content) = filemap.get(&file_path) {
+                fn contents_match(file_path: &Utf8Path, content: &str) -> Result<bool> {
+                    let entry_content = std::fs::read_to_string(file_path)?;
+                    // TODO: hash?
+                    Ok(entry_content == content)
+                }
+                if entry.metadata()?.len() == content.len() as u64
+                    || contents_match(&file_path, content)?
+                {
+                    // same
+                } else {
+                    differing.push(file_path);
+                }
+            } else {
+                extra.push(file_path);
+            }
+        }
+    }
+
+    filemap
+        .keys()
+        .cloned()
+        .collect::<HashSet<_>>()
+        .difference(&all)
+        .for_each(|file| {
+            missing.push(file.to_owned());
+        });
+
+    Ok((differing, extra, missing))
 }
